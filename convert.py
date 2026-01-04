9d69554f74a86c62b4c8617525dbf79663484bbb
EnesTahaTorun <enryu321@gmail.com>
2026-01-04 21:11:45 +0300

    Küçük değişiklikler yapıldı

--- convert.py
+++ convert.py
@@ -8,18 +8,14 @@ print("Found FASTA files:", fasta_files)
 for fasta in fasta_files:
     print("\nProcessing:", fasta)
 
-    # FASTA parse + ID yazdırma
     records = list(SeqIO.parse(fasta, "fasta"))
     for record in records:
         print("Sequence ID:", record.id)
 
-    # Her FASTA için AYRI GenBank dosyası oluştur
     output_file = fasta.replace(".fasta", ".gb")
 
-    # GenBank dönüşüm çağrısı (Biopython şartı sağlanıyor)
     SeqIO.convert(fasta, "fasta", output_file, "genbank")
 
-    # Eğer boş yazarsa bile dosya oluşturulmuş olur
     if not os.path.exists(output_file):
         open(output_file, "w").close()
 
